4a03c4b440a41330b63cd84fcd636e2c813767f1
Yunus Haznedar <yunushaznedar@gmail.com>
2016-08-28 13:16:11 +0300

    Layout Changes
    Updated icons
    Healing on layouts

---
+++ MTMI-master/app/src/main/java/com/mtmi/carapp/LoginActivity.java
@@ -0,0 +1,174 @@
+package com.mtmi.carapp;
+
+
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+
+
+    public EditText mEmailView;
+    public EditText mPasswordView;
+    private ImageView imageView;
+    public final static String MAILKEY="E-posta";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        mEmailView = (EditText) findViewById(R.id.email);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+
+        mEmailView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                imageView.getLayoutParams().height = 120;
+                imageView.getLayoutParams().width = 120;
+            }
+        });
+
+        mPasswordView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                imageView.getLayoutParams().height = 120;
+                imageView.getLayoutParams().width = 120;
+            }
+        });
+
+
+        ImageButton mEmailSignInButton = (ImageButton) findViewById(R.id.email_sign_in_button);
+
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+    }
+
+
+
+
+    public void attemptLogin() {
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email=mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;}
+
+        else if (!isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;}
+
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+
+        if (cancel !=true)
+        {
+            Toast.makeText(this,"Giriş yapılıyor...",Toast.LENGTH_SHORT).show();
+            Intent mesajIntent= new Intent(this, MainActivity.class);
+            mesajIntent.putExtra(MAILKEY,email);
+            startActivity(mesajIntent);
+
+        }
+
+
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+
+        protected void onPostExecute(final Boolean success) {
+
+            if (success) {
+                finish();
+            } else {
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+        }
+        protected void onCancelled() {
+
+
+        }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+}
+
+
+
