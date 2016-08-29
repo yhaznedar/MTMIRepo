feb9391e687b0853e4fede2d2069f20ff71a3389
Yunus <yunushaznedar@gmail.com>
2016-08-29 15:45:12 +0300

    add selam

--- MTMI-master/app/src/main/java/com/mtmi/carapp/LoginActivity.java
+++ MTMI-master/app/src/main/java/com/mtmi/carapp/LoginActivity.java
@@ -128,6 +128,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
+        //selam
         return email.contains("@");
     }
 
