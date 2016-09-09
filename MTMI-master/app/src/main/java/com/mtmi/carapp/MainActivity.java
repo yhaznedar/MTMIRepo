c6ce959aea3c6b0b5e25cb867b8433109add5e63
ss200 <sametdundarr@gmail.com>
2016-09-09 14:56:07 +0300

    Click List
    click list toast message

--- MTMI-master/app/src/main/java/com/mtmi/carapp/MainActivity.java
+++ MTMI-master/app/src/main/java/com/mtmi/carapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mtmi.carapp;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.res.TypedArray;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -101,8 +103,6 @@ public class MainActivity extends AppCompatActivity
 
         initializeData();
         initializeAdapter();
-
-
 
 
 
